35865f24727ddbebc114a75218eb716be10f6a0f
Josefina Mancilla <josefinamancilla@josefinas-mbp.austin.ibm.com>
2018-07-19 15:42:25 -0500

    set up src starter files and server.js

---
+++ src/lib/server.js
@@ -0,0 +1,43 @@
+//THIS IS WHERE WE SET UP  OUR SERVER AND CONNECT IT TO MONGODB
+'use strict'
+
+import cors from 'cors';
+import express from 'express';
+import mongoose from 'mongoose';
+import bodyParser from 'body-parser';
+
+const app = express();
+const router = express.Router();
+
+//env variables
+const PORT = process.env.PORT || 3000;
+const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/mern-starter';
+
+mongoose.Promise = Promise;
+mongoose.connect(MONGODB_URI);
+
+app.use(bodyParser.json(), cors());
+app.use(require('../route/auth-router'));
+
+app.all('*', (req, res) => {
+  console.log('Returning a 404 from the catch-all route');
+  return response.sendStatus(404);
+});
+
+//error middleware
+app.use(require('./error-middleware'));
+
+
+//TURN ON SERVER-----------------------------------------
+export const start = () => {
+  app.listen(PORT, () => {
+    console.log(`Listening on port: ${PORT}`);
+  });
+}
+
+//TURN OFF SERVER-----------------------------------------
+export const stop = () => {
+  app.close(PORT, () => {
+    console.log(`Shut down on port: ${PORT}`);
+  });
+}
